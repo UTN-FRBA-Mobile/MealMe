cbdaa9b640b977f85da945993fa091f111c77e50
Franco Ariel Lopez <franco-ariel.lopez@hotmail.com>
2022-05-20 12:10:09 -0300

    Add repeat password in register, and validations of emails

--- android/app/src/main/java/com/android/mealme/fragments/login/LoginViewModel.kt
+++ android/app/src/main/java/com/android/mealme/fragments/login/LoginViewModel.kt
@@ -65,4 +65,11 @@ class LoginViewModel : ViewModel() {
         }
         return true
     }
+
+    fun enableLogin(): Boolean {
+        if(email.value.isNullOrEmpty() || password.value.isNullOrBlank()){
+            return false;
+        }
+        return true
+    }
 }
